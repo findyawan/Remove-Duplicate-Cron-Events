3a19de941ee96571df31998fe83cd76c5e311e3b
Farras Indyawan <38404173+findyawan@users.noreply.github.com>
2025-02-18 20:36:47 +0700

    Update Remove-Duplicate-Cron-Events.php

--- Remove-Duplicate-Cron-Events.php
+++ Remove-Duplicate-Cron-Events.php
@@ -7,7 +7,12 @@ Author: Farras Indyawan
 Author URI: https://wa.me/6281212833425
  */
 
-function remove_duplicate_cron_events() {
+function remove_duplicate_cron_events($arg = null) {
+    // Jika argumen tidak null, keluarkan pesan debug
+    if ($arg !== null) {
+        error_log("Unexpected argument passed to remove_duplicate_cron_events: " . print_r($arg, true));
+    }
+
     // Mendapatkan semua cron events
     $crons = wp_get_scheduled_events();
 
@@ -37,6 +42,7 @@ function remove_duplicate_cron_events() {
 
 // Jalankan fungsi saat plugin diaktifkan
 register_activation_hook(__FILE__, 'remove_duplicate_cron_events');
+
 
 // Jalan di action init
 add_action('init', 'remove_duplicate_cron_events');
